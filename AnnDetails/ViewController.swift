0abced23ce4aa94db0d9586f96b03dfbe112e232
Ann <annmaryjac1990@gmail.com>
2021-12-09 11:19:23 -0800

    Initial Commit

---
+++ AnnDetails/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AnnDetails
+//
+//  Created by Anil Thomas on 12/9/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
